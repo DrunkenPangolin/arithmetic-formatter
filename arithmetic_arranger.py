15505369e2d103a4ea2f327c28f2a68b2bf2e0ba
Samuel Price <samjprice@yahoo.co.uk>
2021-10-19 20:46:57 +0200

    solved

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,75 +1,43 @@
-def arithmetic_arranger(problems, answers = False):
-    
-    # error too many problems
-    if len(problems) > 5:
-        raise Exception("Error: Too many problems.")
+def arithmetic_arranger(problems, answers=False):
 
-    op1s = []
-    operators = []
-    op2s = []
-    lines = []
-    results = []
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    formatter = ["", "", "", ""]
 
     for problem in problems:
         # split into parts
         [op1, operator, op2] = problem.split()
 
-        # error for -/+
         if operator not in {"-", "+"}:
-            raise Exception("Error: Operator must be '+' or '-'.")
+            return "Error: Operator must be '+' or '-'."
 
-        # error for problem > 4 digits
         if len(op1) > 4 or len(op2) > 4:
-            raise Exception("Error: Numbers cannot be more than four digits.")
+            return "Error: Numbers cannot be more than four digits."
 
-        # error digits only
         try:
-            int(op1)
-            int(op2)
+            int(op1), int(op2)
         except ValueError:
-            raise Exception("Error: Numbers must only contain digits.")
+            return "Error: Numbers must only contain digits."
 
-        # parts
-        op1s.append(op1)
-        operators.append(operator)
-        op2s.append(op2)
-        lines.append((max(len(op1),len(op2))+2)*'-')
-        results.append(eval(problem))
+        # form rows
+        lines = (max(len(op1), len(op2)) + 2) * "-"
+        result = str(eval(problem))
 
-    # results
-    i = 0
+        for i in range(4):
+            if formatter[i] != "":
+                formatter[i] += 4 * " "
+        formatter[0] += (len(lines) - len(op1)) * " " + op1
+        formatter[1] += operator + (len(lines) - 1 - len(op2)) * " " + op2
+        formatter[2] += lines
+        formatter[3] += (len(lines) - len(result)) * " " + result
 
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    line4 = ''
-    
-    for i in range(len(problems)):
-        # line1
-        if i != 0:
-          line1 += 4 * ' '
-        line1 += (len(lines[i]) - len(op1s[i])) * ' ' + str(op1s[i])
+    output = ""
+    for i in range(4):
+        if answers == False and i == 3:
+            break
+        if output != "":
+            output += "\n"
+        output += formatter[i]
 
-
-        # line2
-        if i != 0:
-          line2 += 4 * ' '
-        line2 += str(operator[i]) + (len(lines[i])-1 - len(op2s[i]))*' ' + str(op2s[i])    
-
-        # line3
-        if i != 0:
-          line3 += 4 * ' '
-        line3 += lines[i]
-
-        # line4
-        if i != 0:
-          line4 += 4 * ' '
-        line4 += (len(lines[i])-1 - len(str(results[i])))*' ' + str(results[i])
-
-        print(line1, line2, line3, line4)
-
-    arranged_outcome = line1 + '\n' + line2 + '\n' + line3
-    if answers == True:
-      arranged_outcome += '\n' + line4
-        
-    return arranged_outcome
+    return output
